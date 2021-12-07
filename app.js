a70f7d245cbf9a3c5ea4f34d93fae3a892484929
Lucas Wolf <lwolf5@uco.edu>
2021-12-07 13:20:26 -0600

    added building entrances

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 const UCO = {lat: 35.6575, lng: -97.4709};
 
+const MCS_HAR1 = {lat: 35.65398610414391, lng: -97.47304078722226};
+const MSC_NON_HAR1 = {lat: 35.65424327694166, lng: -97.47289594793779};
+const MSC_NON_HAR2 = {lat: 35.65387386321416, lng: -97.47253116751762};
+
+const UC_HAR1 = {lat: 35.65438078536891, lng: -97.4714516326025}; // first floor
+const UC_HAR2 = {lat: 35.6546946216972, lng: -97.47120218716813}; // first floor
+const UC_HAR3 = {lat: 35.655079734496105, lng: -97.47184606951829}; // first floor
+const UC_HAR4 = {lat: 35.65564419818894, lng: -97.47177230877114}; // first floor
+const UC_HAR5 = {lat: 35.65573225141894, lng: -97.47165021187153}; // second floor
+const UC_HAR6 = {lat: 35.65573007203257, lng: -97.47128543145136}; // second floor
+const UC_HAR7 = {lat: 35.655348678500296, lng: -97.47122105843604}; // second floor
+const UC_NON_HAR1 = {lat: 35.65468962019296, lng: -97.47175219220264}; // first floor
+const UC_NON_HAR2 = {lat: 35.65507950487029, lng: -97.47121061062671}; // first floor
+const UC_NON_HAR2 = {lat: 35.65556879727545, lng: -97.4718165088278}; // second floor
+
+const LIB_HAR1 = {lat: 35.65836414769805, lng: -97.47334064142974};
+const LIB_HAR2 = {lat: 35.657987707399535, lng: -97.47371536172811};
+
 function initMap(){
 	var options = {
 		center: UCO,
